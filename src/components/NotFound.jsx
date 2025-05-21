6323ce5d2b90e683d5d886420672fffaa857b1c5
BEDHIAFI Okba <101400944+oookbaaa@users.noreply.github.com>
2025-05-21 18:51:55 +0100

    Update NotFound.jsx

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
-const NotFound = () => {
-  return (
-    <div>
-        <h1>404 Not Found</h1>
-    </div>
-  )
+class NotFound extends React.Component {
+  render() {
+    return (
+      <div>
+          <h1>404 Not Found</h1>
+      </div>
+    )
+  }
 }
 
 export default NotFound
