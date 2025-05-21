a585b216f0100196a47e9268501f179c529e130a
BEDHIAFI Okba <okba.bedhiafi@esprit.tn>
2025-05-21 16:34:19 +0100

    donee

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 const NavigationBar = () => {
   return (
     <div>
-        <Link to="/home">Home</Link> <span>|</span>
+        <NavLink to="/home" style={({ isActive }) => isActive ? { textDecoration: 'underline' } : {}}>Home</NavLink> <span>|</span>
         {/* <Link to="/notes">Notes</Link> <span>|</span>
         <Link to="/list">List</Link> */}
     </div>
